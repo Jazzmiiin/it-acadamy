38e920b637b1d75da1373956b68da7a640659991
Jazzmiiin <tursynbekovajasmin@gmail.com>
2022-10-24 20:23:17 +0600

    jgjhg

---
+++ index.js
@@ -0,0 +1,48 @@
+// for
+
+for (let i = 0; i <= 20; i++) {
+    if(i % 2 == 0) {
+        console.log(i);
+    }
+ }
+
+
+//for of
+
+// for (let i = 0; i < 1; i++) {
+//     let quetionOne = prompt('Какой жанр фильмов вы предпочитаете?'),
+//         quetionTwo = prompt('Фильм, который не оставил вас равнодушным?'),
+//         quetionThree = prompt('Какова мораль этого фильма?'),
+//         quetionFour = prompt('Посоветуйте похожие фильмы?');
+// };
+
+
+
+//obj
+
+// var obj = {
+// 	'Минск': 'Беларусь',
+// 	'Москва': 'Россия',
+// 	'Киев': 'Украина'
+// };
+
+// for (var key in obj) {
+// 	alert(key + ' - это ' + obj[key] + '.')
+// };
+
+
+// функции
+
+// function checkAge(age) {
+//     if (age > 17 && 18) {
+//     return true;
+//     } 
+//     }
+    
+//     var age = prompt('Ваш возраст?');
+    
+//     if (checkAge(age)) {
+//     alert( 'Вы совершеннолетний:)  Доступ разрешон!' );
+//     } else{
+//     alert( 'Вы несовершеннолетний:(  В доступе отказано!' );
+//     }
